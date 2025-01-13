47af85598b0bb9d8869202a098224372317b2fa7
t4r3k <142579274+machine-moon@users.noreply.github.com>
2025-01-13 12:17:58 -0500

    added social-icons mocks, all tests pass.

---
+++ __mocks__/react-social-icons.js
@@ -0,0 +1,3 @@
+module.exports = {
+  SocialIcon: () => 'SocialIcon',
+};
--- jest.config.cjs
+++ jest.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
     "\\.(css|less|scss|sass)$": "<rootDir>/__mocks__/styleMock.js",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js",
+    "^react-social-icons$": "<rootDir>/__mocks__/react-social-icons.js",
   },
   testMatch: ["<rootDir>/src/**/*.{test,spec}.{ts,tsx}"],
 };
