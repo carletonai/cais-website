19aad1c86c13b29d3ff911e57414e3d3ad336727
boshyxd <poisonhick@gmail.com>
2025-01-12 20:56:56 -0500

    update base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/carletonai.github.io/",
+  base: "/cais-web/",
   css: {
     postcss: './postcss.config.cjs'
   }
